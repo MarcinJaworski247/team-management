e6dcfe9906a83df3f67e7d6824b0be77fabd9c49
MarcinJaworski247 <marcinjaworski247@gmail.com>
2022-11-15 22:54:53 +0100

    navbar items

--- src/components/App/TheNavbar.vue
+++ src/components/App/TheNavbar.vue
@@ -1,16 +1,45 @@
 <template>
-  <div class="navbar pt-3">NAVBAR</div>
+  <nav class="navbar pt-3">
+    <ul class="navbar__list">
+      <li class="navbar__list-item">{{ $t("navbar.team") }}</li>
+      <li class="navbar__list-item">{{ $t("navbar.games") }}</li>
+      <li class="navbar__list-item">{{ $t("navbar.stats") }}</li>
+      <li class="navbar__list-item">{{ $t("navbar.schedule") }}</li>
+      <li class="navbar__list-item">{{ $t("navbar.workouts") }}</li>
+    </ul>
+  </nav>
 </template>
 <script lang="ts" setup></script>
 <style lang="scss" scoped>
 .navbar {
   width: 196px;
   height: 100%;
-  background-color: var(--grey-4);
-  opacity: 0.5;
+  top: 60px;
+  background-color: var(--grey-3);
   position: fixed;
   display: flex;
   justify-content: center;
-  top: 60px;
+
+  &__list {
+    margin-top: 64px;
+    list-style: none;
+    text-transform: uppercase;
+    font-size: 1.8rem;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+  }
+
+  &__list-item {
+    cursor: pointer;
+    padding: 8px 16px;
+    letter-spacing: 1.3px;
+
+    &:hover {
+      color: var(--orange-8);
+      background-color: var(--grey-2);
+      border-radius: 9px;
+    }
+  }
 }
 </style>
